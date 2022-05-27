439c76caa94973dd79cd516a11670797ff7bcdbb
Adapt99 <91166114+Adapt99@users.noreply.github.com>
2022-05-27 12:05:46 -0400

    Added Printf to format output and \n at end of sentences

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ func main() {
 	//Declare "const", value CAN NOT change
 	const conferenceTickets = 50
 	var remainingTickets = 50
-
+	//Printf(Prints formatted data, some text with a variable %s, myVariable)
+	//Added \n at end of sentence to create a new line
+	fmt.Printf("Welcome to our %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	//Println(prints the message in a new line)
-	fmt.Println("Welcome to our", conferenceName, "booking application")
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 }
