3b93f97d641eff4c5c75c6def09640c45c1a215c
Adapt99 <91166114+Adapt99@users.noreply.github.com>
2022-05-27 09:38:19 -0400

    Added comments to explain SLOC significant lines of code

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 //Declare "main" package (package is a way to group functions, and it's made up of all the files in the same directory)
-
 package main
 
 //Import the popular "fmt" package, which contains functions for formatting text, including printing to the console.
-
 import "fmt"
 
 //Implement a "main" function to print a message to the console. A "main" function executes by default when you run the "main" package
