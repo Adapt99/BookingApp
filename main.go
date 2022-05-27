fdb7e181e5018852dcd7bde071a12000a6f02877
Adapt99 <91166114+Adapt99@users.noreply.github.com>
2022-05-27 09:35:23 -0400

    Added comments to explain SLOC significant lines of code

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
+//Declare "main" package (package is a way to group functions, and it's made up of all the files in the same directory)
+
 package main
+
+//Import the popular "fmt" package, which contains functions for formatting text, including printing to the console.
 
 import "fmt"
 
+//Implement a "main" function to print a message to the console. A "main" function executes by default when you run the "main" package
 func main() {
+	//Println(prints the message in a new line)
 	fmt.Println("Welcome to our booking application")
 	fmt.Println("Get your tickets here to attend")
-
 }
